df3f4142ce622babe4e3337da94a1191508aeda4
Donald Whitfield <donaldwhitfield@icloud.com>
2022-06-02 13:11:30 -0300

    Cleaner Version of Qualys API Script

--- qualysdata.py
+++ qualysdata.py
@@ -4,9 +4,21 @@ __email__     = 'donald.r.whitfield@jci.com'
 __satus__     = 'Development Version'
 
 import os
+import json
 from dotenv import load_dotenv
 import requests
 import xml.etree.ElementTree as ET
+
+
+class QualysAPI:
+    def __init__(self, auth_url, header, file_name, api_endpoint, username, password):
+        self.auth_url = auth_url
+        self.file_name = file_name
+        self.username = username
+        self.password = password
+        self.api_endpoint = api_endpoint
+        self.api_endpoint = api_endpoint
+        self.headers = headers
 
 
 load_dotenv()
@@ -15,24 +27,67 @@ USERNAME = os.getenv("USER_ID")
 PASSWORD = os.getenv("PASSWORD")
 
 
-def login():
+def GetAssets():
 
     #
-    # ---Session Login Code---
+    # ---Pull Qualys Asset Groups for Tenant Associated with Account---
     #
 
+    global headers
+    headers = {"X-Requested-With": "Python Auth"}
+
+    global payload
     payload = {
                'action':'login',
                'username': USERNAME,
                'password': PASSWORD
                }
-    
-    response = requests.post('https://qualysguard.qg2.apps.qualys.eu/api/2.0/fo/scan/', data=payload)
-    print(response)
 
-    # Now that all the hard work was done, we can parse the response.
-    xmlreturn = ET.fromstring(response.text)
-    for elem in xmlreturn.findall('.//TEXT'):
-        print (elem.text) #Prints the "Logged in" message. Not really needed, but reassuring.
 
-login()
+    global auth_url
+    auth_url = "https://qualysguard.qg2.apps.qualys.eu/api/2.0/fo/session/"
+    assets_url = "https://qualysguard.qg2.apps.qualys.eu/api/2.0/fo/asset/group/?action=list"
+
+    filename = "QualysAssetData.xml"
+    file_mode = "w"
+
+    with requests.Session() as session:
+        post = session.post(auth_url, headers=headers, data=payload)
+        r = session.get(assets_url, headers=headers)
+        print(r.text)
+
+    with open(filename, file_mode) as f:
+            f.write(r.text)
+
+'''        
+        file = open("QualysAssetData.xml", "a")
+        file.write(r.text)
+        file.close()
+'''
+
+
+
+def GetSchedScans():
+
+    filename = "QualysNetworksData.xml"
+    file_mode = "w"
+    networks_url = "https://qualysguard.qg2.apps.qualys.eu/api/2.0/fo/schedule/scan/?action=list"
+
+    with requests.Session() as session:
+        post = session.post(auth_url, headers=headers, data=payload)
+        r = session.get(networks_url, headers=headers)
+        print(r.text)
+        
+        with open(filename, file_mode) as f:
+            f.write(r.text)
+            
+            
+'''
+        file = open("QualysNetworksData.xml", "a")
+        file.write(r.text)
+        file.close()
+'''
+
+
+GetAssets()
+GetSchedScans()
